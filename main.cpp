cb18786b0526fc92983de3737b565a14cc585337
Noora Liv <j.czinzoll@ingressy.de>
2025-12-04 21:58:23 +0100

    Add pomodoro and WiFi

--- main.cpp
+++ main.cpp
@@ -1,101 +1,119 @@
-#include <Wire.h>
-#include <LiquidCrystal_I2C.h>
 #include <Arduino.h>
+#include <LiquidCrystal_I2C.h>
+//#include <Wire.h>
+#include <header.h>
+
+const int lbutton = 13;
+const int button = 8;
+const int rbutton = 7;
+const int led = 2;
+
+int pomodoro_time = 25*60;
+bool active = true;
+int site = 0;
 
 LiquidCrystal_I2C lcd(0x27, 16, 2);
 
-const int lbutton = 2;
-const int button = 0;
-const int rbutton = 4;
-
-int currentStatel;
-int currentState;
-int currentstater;
-int site;
-int pomodoro_time = 25*60;
-
-void pomodoro();
-
 void setup() {
-  pinMode(lbutton, INPUT);
-  pinMode(button, INPUT);
-  pinMode(rbutton, INPUT);
+    lcd.init();
+    lcd.backlight();
 
-  lcd.init();
-  lcd.backlight();
+    lcd.setCursor(0,0);
+    lcd.print("HomeDash startet");
 
-  lcd.setCursor(0,0);
-  lcd.print("HomeDash startet");
+    lcd.setCursor(0,1);
+    lcd.print("Bitte warten ...");
 
-  lcd.setCursor(0,1);
-  lcd.print("Bitte warten ...");
-  delay(1000);
-  site = 0;
+    connectWifi();
+    printIP(lcd);
+
+    pinMode(lbutton, INPUT);
+    pinMode(button, INPUT);
+    pinMode(rbutton, INPUT);
+
 }
 
 void loop() {
-
-  if (site == 0) {
-    lcd.clear();
-    lcd.setCursor(0,0);
-    lcd.print("0");
-    lcd.write(byte(223));
-    lcd.print("C 00%");
-    lcd.setCursor(0,1);
-    lcd.print("0A 0W");
-
-    delay(1000);
-
-    site = 0;
-    currentStatel = digitalRead(lbutton);
-    currentState = digitalRead(button);
-
-    if(currentStatel == HIGH) {
-      site = 1;
-    } else if (currentState == HIGH) {
-      lcd.noBacklight();
+    if (site == 0) {
+        startseite();
     }
-  } else if (site == 1) {
-    lcd.clear();
-    pomodoro();
-  }
-  
+    if (site == 1) {
+        pomodoro();
+    }
+
+    for (int i = 0; i <= 10; i++ ) {
+        delay(100);
+        if (digitalRead(button) == HIGH) {
+            delay(100);
+            break;
+        } if (digitalRead(lbutton) == HIGH) {
+            delay(100);
+            break;
+        }
+    }
+
+    if(digitalRead(lbutton) == HIGH) {
+        site = 1;
+    }
+    if(digitalRead(button) == HIGH && site == 0) {
+        if (active) {
+            lcd.noBacklight();
+            active = false;
+        } else {
+            lcd.backlight();
+            active = true;
+        }
+
+    }
 }
 
 void pomodoro() {
-  lcd.clear();
-  lcd.setCursor(0,0);
-  lcd.print("Pomodoro:");
+    lcd.clear();
+    lcd.setCursor(0,0);
+    lcd.print("Pomodoro:");
 
-  lcd.setCursor(0,1);
-  lcd.print("25min 00sek");
+    lcd.setCursor(0,1);
+    lcd.print("25min 00sek");
 
-  if (digitalRead(button) == HIGH) {
-    for (int sec = pomodoro_time; sec >= 0; sec--) {
-      int m = sec/60;
-      int s = sec % 60;
+   if (digitalRead(button) == HIGH) {
+        for (int sec = pomodoro_time; sec >= 0; sec--) {
+            int m = sec/60;
+            int s = sec % 60;
 
-      lcd.clear();
-      lcd.setCursor(0,0);
-      lcd.print("Pomodoro:");
+            lcd.clear();
+            lcd.setCursor(0,0);
+            lcd.print("Pomodoro:");
 
-      lcd.setCursor(0,1);
-      if (m < 10) lcd.print("0");
-      lcd.print(m);
-      lcd.print("min ");
-      if (s < 10) lcd.print("0");
-      lcd.print(s);
-      lcd.print("sek");
+            lcd.setCursor(0,1);
+            if (m < 10) lcd.print("0");
+            lcd.print(m);
+            lcd.print("min ");
+            if (s < 10) lcd.print("0");
+            lcd.print(s);
+            lcd.print("sek");
 
-      delay(1000);
-      currentState = digitalRead(lbutton);
-      if(currentState == HIGH) {
-        break;
-      }
+            delay(1000);
+            if(digitalRead(button)== HIGH) {
+                break;
+            }
+        }
     }
-  }
 
-  lcd.clear();
-  lcd.setCursor(0,1);
-  lcd.print("Fertig!");
+    if (pomodoro_time == 0) {
+        lcd.clear();
+        lcd.setCursor(0,1);
+        lcd.print("Fertig!");
+        while (pomodoro_time == 0) {
+            digitalWrite(led, HIGH);
+            delay(500);
+            digitalWrite(led, LOW);
+            delay(500);
+        }
+    }
+}
+
+void startseite() {
+    lcd.clear();
+    lcd.setCursor(0,0);
+    lcd.print("Startseite");
 }
